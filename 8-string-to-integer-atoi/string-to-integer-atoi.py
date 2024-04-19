c0ecdaebaee860ddef32458afef80ff3e109f243
Chew Tze Han <114734857+ChewTzeHan@users.noreply.github.com>
2024-04-19 19:46:54 +0800

    Time: 43 ms (23.98%) | Memory: 16.7 MB (32.60%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -11,19 +11,18 @@ class Solution:
         for i in s.strip():
             if i.isnumeric() == False:
                 if i == '-' and opp_found == False:
-                    
-                    if num_str != '':
-                        break
                     neg = True
                     opp_found = True
+                    if num_str != '':
+                        neg = False
+                        break
                     continue
 
                 elif i == '+' and opp_found == False:
-                    
-                    if num_str != '':
-                        break
                     neg = False
                     opp_found = True
+                    if num_str != '':
+                        break
                     continue
 
                 else:
